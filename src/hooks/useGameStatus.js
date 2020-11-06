24ff178c8cbc55bc9d70e3e9da1f811adde1fb20
Jasur <ibraimov0409@mail.ru>
2020-11-06 08:40:31 +0600

    final

---
+++ src/hooks/useGameStatus.js
@@ -0,0 +1,19 @@
+import { useState, useEffect, useCallback } from 'react'
+export const useGameStatus = (rowsCleared) => {
+	const [score, setScore] = useState(0)
+	const [rows, setRows] = useState(0)
+	const [level, setLevel] = useState(0)
+	const calcScore = useCallback(() => {
+		// We have score
+		const linePoints = [40, 100, 300, 1200]
+		if (rowsCleared > 0) {
+			setScore((prev) => prev + linePoints[rowsCleared - 1] * (level + 1))
+			setRows((prev) => prev + rowsCleared)
+		}
+	}, [level, rowsCleared])
+
+	useEffect(() => {
+		calcScore()
+	}, [calcScore, rowsCleared, score])
+	return [score, setScore, rows, setRows, level, setLevel]
+}
